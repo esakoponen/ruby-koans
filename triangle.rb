bb4e5e0dcc1185d92a96e0992ad46ccd8a70a3bd
Esa Koponen <esa.koponen@jotautomation.com>
2014-11-03 13:31:02 +0200

    About tdd
    Signed-off-by: Esa Koponen <esa.koponen@jotautomation.com>

--- triangle.rb
+++ triangle.rb
@@ -14,15 +14,26 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
-  if a == b and b == c then
-    puts "equilateral #{a} #{b} #{c}"
+  # Make sure that the longest edge is shorter than the sum
+  # of the two shorter edges.
+  max_value = 0
+  sum = 0
+  [a, b, c].each do |i|
+    sum += i
+    if i > max_value then
+      max_value = i
+    end
+  end
+
+  if a <= 0 or b <= 0 or c <= 0 then
+    raise TriangleError
+  elsif sum - max_value <= max_value then
+    raise TriangleError
+  elsif a == b and b == c then
     :equilateral
   elsif a != b and a != c and b != c then
-    puts "scalene #{a} #{b} #{c}"
     :scalene
   else
-    puts "isosceles #{a} #{b} #{c}"
     :isosceles
   end
 end
